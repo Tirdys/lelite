a32f37bb0da5dc9655e9e0eda17eee8a6b9efc98
KevZh-hub <55091168+KevZh-hub@users.noreply.github.com>
2019-12-05 18:02:51 +0100

    Modify exemple.php
    css added

--- example.php
+++ example.php
@@ -4,52 +4,52 @@
 <head>
   <meta charset="utf-8">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-  <link href="main.css" rel="stylesheet">
+  <link href="cover.css" rel="stylesheet">
   <title>Meteo</title>
   <meta name="description" content="The HTML5 Herald">
   <meta name="author" content="SitePoint">
-
 </head>
 
 <body>
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+    <header class="masthead mb-auto">
+        <div class="inner">
+            <h3 class="masthead-brand">API</h3>
+            <nav class="nav nav-masthead justify-content-center">
+                <a class="nav-link active" href="#">Accueil</a>
+            </nav>
+        </div>
+    </header>
 
+<div class="body">
 <?php
 
 $ville = $_POST['meteo'];
 
-//turning off low level notices
 error_reporting(E_ALL ^ E_NOTICE);
 
-//instantiate the class
 include('weatherstack.class.php');
 $weather = new weatherStack();
 
-//set the end point to use
-//free plan uses the 'current' endpoint
-//we can set it with 'current' or 'c' shorthand
 $weather->setEndPoint('c');
 
-//set the query parameter
 $weather->setParam('query',$ville);
 
-//optionally set the units
 $weather->setParam('units','m');
 
-//get the response
 $weather->getResponse();
 
-//show specific response data
 echo 'La température actuelle à '.$weather->response->location->name.', '
     .$weather->response->location->region.' est de '
     .$weather->response->current->temperature
     .' degrés ('.$weather->response->request->unit.') la date : '
     .$weather->response->location->localtime;
-    
 
-echo '<hr>';
+
+echo '<br><br>';
 
 ?><u><?php echo 'Informations sur le vent :';?></u><br><br><?php echo 'La Vitesse du vent est de ' .$weather->response->current->wind_speed. 'Km/h et un vent qui vient du ' .$weather->response->current->wind_dir.' avec un un axe de '.$weather->response->current->wind_degree.'°';
-?><br><hr><?php echo 'L\'indice UV est de '.$weather->response->current->uv_index;
+?><br><br><?php echo 'L\'indice UV est de '.$weather->response->current->uv_index;
 
 ?>
 
@@ -66,7 +66,12 @@ echo '<hr>';
       _aqiFeed({  container:"city-aqi-container",  city: ville, display:"La qualité de l'air à %cityname est de %aqi<br><small>on  %date</small>", lang:"fr" });
         
   </script>
-
-  
+</div>
+    <footer class="mastfoot mt-auto">
+        <div class="inner">
+            <p>Une création de DUPLOUY Antoine et ZHANG Kevin</p>
+        </div>
+    </footer>
+</div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,5 +1,4 @@
-
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -11,10 +10,8 @@
 
     <link rel="canonical" href="https://getbootstrap.com/docs/4.4/examples/cover/">
 
-    <!-- Bootstrap core CSS -->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-    <!-- Favicons -->
 <link rel="apple-touch-icon" href="/docs/4.4/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
 <link rel="icon" href="/docs/4.4/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
 <link rel="icon" href="/docs/4.4/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
@@ -24,7 +21,6 @@
 <meta name="msapplication-config" content="/docs/4.4/assets/img/favicons/browserconfig.xml">
 <meta name="theme-color" content="#563d7c">
 
-    <!-- Custom styles for this template -->
     <link href="cover.css" rel="stylesheet">
   </head>
   <body class="text-center">
